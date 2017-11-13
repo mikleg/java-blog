115322d250d54b0f45ced79222859170d36ed809
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-11-12 21:01:32 -0800

    Thymeleaf changed from 2.0 on 3.0

--- src/main/java/mikleg/myblog/controllers/HomeController.java
+++ src/main/java/mikleg/myblog/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package mikleg.myblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+     //   model.addAttribute("title", "Mikle's site");
         return "index";
     }
 }
